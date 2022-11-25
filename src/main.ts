3cc51d0b7ec2fdcb3290d5a32cc14529a2276fca
Michael Arnaldi <arnaldimichael@gmail.com>
2022-11-25 01:29:29 +0000

    add language service

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
-import { Context, Effect, Layer, Logger, pipe } from "effect"
+import { Context, Effect, Layer, Logger, pipe } from "effect";
 
 export interface Name {
-  getName: Effect.Effect<never, never, string>
+  getName: Effect.Effect<never, never, string>;
 }
 
-export const Name = Context.Tag<Name>()
+export const Name: Context.Tag<Name> = Context.Tag<Name>();
 
-export const program = Effect.gen(function*($) {
-  const { getName } = yield* $(Effect.service(Name))
+export const program: Effect.Effect<Name, never, void> = Effect.gen(function* ($) {
+  const { getName } = yield* $(Effect.service(Name));
 
-  yield* $(Effect.log(`Hello ${yield* $(getName)}`))
-})
+  yield* $(Effect.log(`Hello ${yield* $(getName)}`));
+});
 
-export const NameLive = Layer.fromEffect(Name)(
+export const NameLive: Layer.Layer<never, never, Name> = Layer.fromEffect(Name)(
   Effect.sync(() => ({
-    getName: Effect.succeed("Mike")
+    getName: Effect.succeed("Mike"),
   }))
-)
+);
 
 pipe(
   program,
   Effect.provideLayer(NameLive),
   Effect.provideLayer(Logger.console()),
   Effect.unsafeFork
-)
+);
