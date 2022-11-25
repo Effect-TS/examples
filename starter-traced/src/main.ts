e0ba872a6613c54e069a2b64bf9716c06c8263bc
Michael Arnaldi <arnaldimichael@gmail.com>
2022-11-25 15:46:29 +0000

    add traced example

---
+++ starter-traced/src/main.ts
@@ -0,0 +1,31 @@
+import { Cause, Context, Effect, Exit, Layer, Logger, pipe } from "effect";
+
+export interface Name {
+  getName: Effect.Effect<never, never, string>;
+}
+
+export const Name: Context.Tag<Name> = Context.Tag<Name>();
+
+export const program: Effect.Effect<Name, never, void> = Effect.gen(function* ($) {
+  const { getName } = yield* $(Effect.service(Name));
+
+  yield* $(Effect.log(`Hello ${yield* $(getName)}`));
+  yield* $(Effect.die("Error"));
+});
+
+export const NameLive: Layer.Layer<never, never, Name> = Layer.fromEffect(Name)(
+  Effect.sync(() => ({
+    getName: Effect.succeed("Mike"),
+  }))
+);
+
+pipe(
+  program,
+  Effect.provideLayer(NameLive),
+  Effect.provideLayer(Logger.console()),
+  Effect.unsafeRunPromiseExit
+).then((exit) => {
+  if (Exit.isFailure(exit)) {
+    console.error(Cause.pretty()(exit.cause));
+  }
+});
