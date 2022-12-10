267bb1c422daee5da0032b84cdcd721ae9b52549
Michael Arnaldi <arnaldimichael@gmail.com>
2022-12-10 13:41:29 +0000

    prettier pass

--- starter-remix-tsc/app/routes/index.tsx
+++ starter-remix-tsc/app/routes/index.tsx
@@ -1,11 +1,11 @@
 import { useLoaderData } from "@remix-run/react";
-import { Effect } from "effect/io"
+import { Effect } from "effect/io";
 import { makeLoader, requestURL } from "~/utils";
 
 export const loader = makeLoader(
   Effect.gen(function* ($) {
     const { pathname } = yield* $(requestURL);
-    
+
     return yield* $(
       Effect.succeed({ message: `hello world from ${pathname}` })
     );
