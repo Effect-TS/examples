0689a9b34d661a95c2ba77eb0cce7a19391e81e0
Michael Arnaldi <arnaldimichael@gmail.com>
2022-12-12 23:17:36 +0000

    use plugin from npm

--- starter-remix-tsc/plugins-dev/out/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-if (process.argv[2] === "build") {
-    process.argv.pop();
-    require("./build");
-}
-else {
-    process.argv.pop();
-    require("./dev");
-}
--- starter-remix-tsc/plugins-dev/src/index.ts
+++
@@ -1,7 +0,0 @@
-if (process.argv[2] === "build") {
-  process.argv.pop();
-  require("./build");
-} else {
-  process.argv.pop();
-  require("./dev");
-}
