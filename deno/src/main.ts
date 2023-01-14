c866dab71c3f96a28f2514c21967201e7425c907
Michael Arnaldi <arnaldimichael@gmail.com>
2023-01-14 18:13:57 +0000

    improve example

--- deno/src/main.ts
+++ deno/src/main.ts
@@ -1,38 +1,33 @@
 import { Duration, Effect, pipe, Queue, runMain } from '@/lib';
 
-const main = pipe(
-  Effect.gen(function* ($) {
-    const queue = yield* $(Queue.unbounded<number>());
+const main = Effect.gen(function* ($) {
+  const queue = yield* $(Queue.unbounded<number>());
 
-    for (let i = 0; i < 3; i++) {
-      yield* $(pipe(
-        Effect.gen(function* ($) {
-          yield* $(Effect.gen(function* ($) {
-            while (true) {
-              const n = yield* $(Queue.take(queue));
-              yield* $(Effect.log(`got: ${n}`));
-            }
-          }));
-        }),
-        Effect.onInterrupt(() => Effect.log(`interrupted pull`)),
-        Effect.forkScoped,
-      ));
-    }
-
-    return queue;
-  }),
-  Effect.flatMap((queue) =>
-    pipe(
+  for (let i = 0; i < 3; i++) {
+    yield* $(pipe(
       Effect.gen(function* ($) {
-        let n = 0;
-        while (true) {
-          yield* $(Effect.sleep(Duration.millis(500)));
-          yield* $(Queue.offer(n++)(queue));
-        }
+        yield* $(Effect.gen(function* ($) {
+          while (true) {
+            const n = yield* $(Queue.take(queue));
+            yield* $(Effect.log(`got: ${n}`));
+          }
+        }));
       }),
-      Effect.onInterrupt(() => Effect.log(`interrupted push`)),
-    )
-  ),
-);
+      Effect.onInterrupt(() => Effect.log(`interrupted pull`)),
+      Effect.forkScoped,
+    ));
+  }
+
+  yield* $(pipe(
+    Effect.gen(function* ($) {
+      let n = 0;
+      while (true) {
+        yield* $(Effect.sleep(Duration.millis(500)));
+        yield* $(Queue.offer(n++)(queue));
+      }
+    }),
+    Effect.onInterrupt(() => Effect.log(`interrupted push`)),
+  ));
+});
 
 runMain(main);
