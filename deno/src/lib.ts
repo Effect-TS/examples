5c30fd6d2298ecc718ddba81e72977047a627feb
Michael Arnaldi <arnaldimichael@gmail.com>
2023-01-14 17:54:53 +0000

    improve runMain

--- deno/src/lib.ts
+++ deno/src/lib.ts
@@ -1,29 +1,56 @@
 import * as Cause from 'npm:@effect/io/Cause';
 import * as Effect from 'npm:@effect/io/Effect';
 import * as Fiber from 'npm:@effect/io/Fiber';
+import * as Exit from 'npm:@effect/io/Exit';
 import { pipe } from 'npm:@fp-ts/data/Function';
+import { millis } from 'npm:@fp-ts/data/Duration';
 
 export const runMain = <E, A>(effect: Effect.Effect<never, E, A>) => {
-  const fiber = Effect.unsafeFork(effect);
+  let interrupt = false;
+
+  const fiber = pipe(
+    effect,
+    Effect.matchCauseEffect(
+      (cause) =>
+        Cause.isInterruptedOnly(cause) ? Effect.unit() : pipe(
+          Effect.logErrorCause(cause),
+          Effect.flatMap(() => Effect.failCause(cause)),
+        ),
+      () => Effect.unit(),
+    ),
+    Effect.unsafeFork,
+  );
+
+  fiber.unsafeAddObserver((exit) => {
+    if (!interrupt) {
+      if (Exit.isFailure(exit)) {
+        Deno.exit(1);
+      } else {
+        Deno.exit(0);
+      }
+    }
+  });
 
   Deno.addSignalListener(
     'SIGINT',
-    () =>
+    () => {
+      interrupt = true;
       pipe(
         Fiber.interrupt(fiber),
         Effect.flatten,
-        Effect.matchCauseEffect(
-          (cause) =>
-            Cause.isInterruptedOnly(cause)
-              ? Effect.unit()
-              : Effect.failCause(cause),
-          () => Effect.unit(),
-        ),
-        Effect.unsafeRunPromise,
-      ),
+        Effect.unsafeFork,
+      ).unsafeAddObserver((exit) => {
+        if (Exit.isFailure(exit)) {
+          Deno.exit(1);
+        } else {
+          Deno.exit(0);
+        }
+      });
+    },
   );
 };
 
+export * as Exit from 'npm:@effect/io/Exit';
 export * as Queue from 'npm:@effect/io/Queue';
 export * as Duration from 'npm:@fp-ts/data/Duration';
 export * as Effect from 'npm:@effect/io/Effect';
