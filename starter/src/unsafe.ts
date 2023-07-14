9673e9fc52b04ffb9526e52066d9eb390e36c62e
Alen Kis <16632804+alenkis@users.noreply.github.com>
2023-07-14 16:11:03 -0600

    use pipe method

--- starter/src/unsafe.ts
+++ starter/src/unsafe.ts
@@ -1,9 +1,8 @@
-import { Effect, pipe } from "effect";
+import { Effect } from "effect";
 import { program } from "~/program";
 import { NameServiceLive } from "~/services/name";
 
-pipe(
-  program,
+program.pipe(
   Effect.provideLayer(NameServiceLive),
   Effect.tapErrorCause(Effect.logCause({ level: "Error" })),
   Effect.runFork
