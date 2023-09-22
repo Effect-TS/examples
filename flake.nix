c53594bb892f7c11d51ef518d64d394ab4358c4b
Sebastian Lorenz <fubhy@fubhy.com>
2023-09-22 15:52:31 +0200

    add corepack

--- flake.nix
+++ flake.nix
@@ -8,13 +8,16 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        corepack = pkgs.runCommand "corepack-enable" {} ''
+          mkdir -p $out/bin
+          ${pkgs.nodejs_20}/bin/corepack enable --install-directory $out/bin
+        '';
       in
       {
-
         devShell = with pkgs; pkgs.mkShell {
           buildInputs = [
             nodejs_20
-            (yarn.override { nodejs = nodejs_20; })
+            corepack
           ];
         };
 
