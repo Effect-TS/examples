f1548b9826212069caa884a56ca3d73ab9890ddf
Johannes Schickling <schickling.j@gmail.com>
2023-09-22 15:35:22 +0200

    pin flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
