3f6aa1e06f3d5f6e021bc26f106714c619a882f2
Johannes Schickling <schickling.j@gmail.com>
2023-09-24 17:44:31 +0200

    simplify

--- cli/src/wttr.ts
+++ cli/src/wttr.ts
@@ -1,4 +1,4 @@
-import { Effect, Option, Cause } from 'effect'
+import { Effect, Option, Layer } from 'effect'
 import * as Http from '@effect/platform-node/HttpClient'
 import * as Node from '@effect/platform-node/Runtime'
 import * as Command from '@effect/cli/Command'
@@ -17,12 +17,10 @@ const WttrWeatherSchema = Schema.struct({
   mintempC: Schema.numberFromString(Schema.string),
   mintempF: Schema.numberFromString(Schema.string),
   sunHour: Schema.numberFromString(Schema.string),
-  uvIndex: Schema.numberFromString(Schema.string)
+  uvIndex: Schema.numberFromString(Schema.string),
 })
 
-const WttrResponseSchema = Schema.struct({
-  weather: Schema.array(WttrWeatherSchema)
-})
+const WttrResponseSchema = Schema.struct({ weather: Schema.array(WttrWeatherSchema) })
 
 // Defines the command line interface for the `weather` command.
 const cli = Cli.make({
@@ -35,17 +33,19 @@ const cli = Cli.make({
       // Allow the user to override the default `wttr.in` service with the `--url` option.
       url: Options.withDefault(Options.text('url'), 'https://wttr.in'),
     }),
-  })
+  }),
 })
 
 // Runs the previously defined command line interface with the given arguments.
-const main = (argv: string[]) => Cli.run(cli, argv, ({ options, args }) => {
+const main = Cli.run(cli, process.argv.slice(2), ({ options, args }) =>
   // Based on the given options and arguments, you could invoke different (sub)commands here or
   // pass different parameters to your command function. In this example, we only have a single
   // command, so we just invoke it directly.
-  return Effect.gen(function* ($) {
-    const defaultClient = yield* $(Http.client.Client)
-    const wttrClient = defaultClient.pipe(
+  Effect.gen(function* ($) {
+    const httpClient = yield* $(Http.client.Client)
+
+    // We're preparing a custom http client that will be used to send the request to the wttr.in service.
+    const wttrClient = httpClient.pipe(
       // Always prepend the service url to all requests. This makes it more convenient to use the client.
       Http.client.mapRequest(Http.request.prependUrl(options.url)),
       // Tell the wttr.in service to return json.
@@ -55,29 +55,26 @@ const main = (argv: string[]) => Cli.run(cli, argv, ({ options, args }) => {
       // Decode all responses using the `WttrResponseSchema` schema.
       Http.client.mapEffect(Http.response.schemaBodyJson(WttrResponseSchema)),
     )
-  
+
     // Create the weather request for the provided location or the current location if none was provided.
     const wttrRequest = Option.match(args, {
       onNone: () => Http.request.get('/'),
       onSome: (_) => Http.request.get(`/${encodeURIComponent(_)}`),
     })
 
-    // Send the request and wait for the response, then extract the `.weather` property from the parsed object.
-    const wttrResponse = yield* $(wttrClient(wttrRequest), Effect.map((_) => _.weather))
-  
+    // Send the request and wait for the response
+    const wttrResponse = yield* $(wttrClient(wttrRequest))
+
     // TODO: Pretty print the weather.
-    yield* $(Effect.log(wttrResponse))
-  })
-})
-
-// Obtain the command line arguments and invoke the main function.
-const program = Effect.sync(() => process.argv.slice(2)).pipe(Effect.flatMap((_) => main(_)))
-
-// Provide the required layers (a.k.a. dependency injection) to the program.
-const runnable = program.pipe(
-  Effect.provideSomeLayer(Console.layer),
-  Effect.provideSomeLayer(Http.client.layer)
+    yield* $(Effect.log(wttrResponse.weather))
+  }),
 )
 
 // Run the program and pretty print any errors.
-Node.runMain(runnable.pipe(Effect.tapErrorCause((_) => Effect.logError(Cause.pretty(_)))))
+Node.runMain(
+  main.pipe(
+    // Provide the required layers (a.k.a. dependency injection) to the program.
+    Effect.provideSomeLayer(Layer.mergeAll(Console.layer, Http.client.layer)),
+    Effect.tapErrorCause(Effect.logError),
+  ),
+)
