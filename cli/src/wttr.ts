8edfb3b74c970fece6654fa93e1f697ddbb53969
Sebastian Lorenz <fubhy@fubhy.com>
2023-09-24 17:20:41 +0200

    refactor for simplicity

--- cli/src/wttr.ts
+++ cli/src/wttr.ts
@@ -24,6 +24,7 @@ const WttrResponseSchema = Schema.struct({
   weather: Schema.array(WttrWeatherSchema)
 })
 
+// Defines the command line interface for the `weather` command.
 const cli = Cli.make({
   name: 'Weather',
   version: '1.2.3',
@@ -31,40 +32,49 @@ const cli = Cli.make({
     // Accept 0 or 1 arguments for the location and convert it to a `Option<string>`.
     args: Args.between(Args.text({ name: 'location' }), 0, 1).pipe(Args.map(Option.fromIterable)),
     options: Options.all({
+      // Allow the user to override the default `wttr.in` service url.
       url: Options.withDefault(Options.text('url'), 'https://wttr.in'),
     }),
   })
 })
 
-Node.runMain(Effect.sync(() => process.argv.slice(2)).pipe(
-  Effect.flatMap((args) =>
-    Cli.run(cli, args, ({ options: { url }, args: location }) => {
-      const program = Effect.gen(function* ($) {
-        const defaultClient = yield* $(Http.client.Client)
-        const wttrClient = defaultClient.pipe(
-          // Always prepend the service url to all requests. This makes it more convenient to use the client.
-          Http.client.mapRequest(Http.request.prependUrl(url)),
-          // Tell the wttr.in service to return json.
-          Http.client.mapRequest(Http.request.appendUrlParam('format', 'j1')),
-          // Only accept status responses with a `2xx` status code. Fail otherwise.
-          Http.client.filterStatusOk,
-          // Decode all responses using the `WttrResponseSchema` schema.
-          Http.client.mapEffect(Http.response.schemaBodyJson(WttrResponseSchema)),
-        );
+// Runs the previously defined command line interface with the given arguments.
+const main = (argv: string[]) => Cli.run(cli, argv, ({ options, args }) => {
+  // Based on the given options and arguments, you could invoke different (sub)commands here or
+  // pass different parameters to your command function. In this example, we only have a single
+  // command, so we just invoke it directly.
+  return Effect.gen(function* ($) {
+    const defaultClient = yield* $(Http.client.Client)
+    const wttrClient = defaultClient.pipe(
+      // Always prepend the service url to all requests. This makes it more convenient to use the client.
+      Http.client.mapRequest(Http.request.prependUrl(options.url)),
+      // Tell the wttr.in service to return json.
+      Http.client.mapRequest(Http.request.appendUrlParam('format', 'j1')),
+      // Only accept status responses with a `2xx` status code. Fail otherwise.
+      Http.client.filterStatusOk,
+      // Decode all responses using the `WttrResponseSchema` schema.
+      Http.client.mapEffect(Http.response.schemaBodyJson(WttrResponseSchema)),
+    );
+  
+    // Request the weather for the provided location or the current location if none was provided.
+    const wttrResponse = yield* $(Option.match(args, {
+      onNone: () => Http.request.get('/'),
+      onSome: (_) => Http.request.get(`/${encodeURIComponent(_)}`),
+    }).pipe(wttrClient, Effect.map(_ => _.weather)));
+  
+    // TODO: Pretty print the weather.
+    yield* $(Effect.log(wttrResponse));
+  });
+})
 
-        // Request the weather for the provided location or the current location if none was provided.
-        const wttrResponse = yield* $(Option.match(location, {
-          onNone: () => Http.request.get('/'),
-          onSome: (_) => Http.request.get(`/${encodeURIComponent(_)}`),
-        }).pipe(wttrClient, Effect.map(_ => _.weather)));
+// Obtain the command line arguments and invoke the main function.
+const program = Effect.sync(() => process.argv.slice(2)).pipe(Effect.flatMap((_) => main(_)));
 
-        // TODO: Pretty print the weather.
-        yield* $(Effect.log(wttrResponse));
-      });
+// Provide the required layers (a.k.a. dependency injection) to the program.
+const runnable = program.pipe(
+  Effect.provideSomeLayer(Console.layer),
+  Effect.provideSomeLayer(Http.client.layer)
+)
 
-      return Effect.provideLayer(program, Http.client.layer)
-    })
-  ),
-  Effect.provideLayer(Console.layer),
-  Effect.tapErrorCause((_) => Effect.logError(Cause.pretty(_)))
-))
+// Run the program and pretty print any errors.
+Node.runMain(runnable.pipe(Effect.tapErrorCause((_) => Effect.logError(Cause.pretty(_)))))
