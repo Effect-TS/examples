7c96fd53e477e43748f0eb80337290e2f91719d0
Maxwell Brown <maxwellbrown1990@gmail.com>
2024-10-31 09:15:06 -0400

    update create-effect-app cli package

--- packages/create-effect-app/src/GitHub.ts
+++ packages/create-effect-app/src/GitHub.ts
@@ -5,57 +5,53 @@ import * as HttpClient from "@effect/platform/HttpClient"
 import * as HttpClientRequest from "@effect/platform/HttpClientRequest"
 import * as HttpClientResponse from "@effect/platform/HttpClientResponse"
 import * as Effect from "effect/Effect"
-import * as Layer from "effect/Layer"
 import * as Stream from "effect/Stream"
 import * as Tar from "tar"
 import type { ExampleConfig, TemplateConfig } from "./Cli.js"
 
-export const make = Effect.gen(function*() {
-  const client = yield* HttpClient.HttpClient
+export class GitHub extends Effect.Service<GitHub>()("app/GitHub", {
+  accessors: true,
+  effect: Effect.gen(function*() {
+    const client = yield* HttpClient.HttpClient
 
-  const codeloadBaseUrl = "https://codeload.github.com"
+    const codeloadBaseUrl = "https://codeload.github.com"
 
-  const codeloadClient = client.pipe(
-    HttpClient.filterStatusOk,
-    HttpClient.mapRequest(HttpClientRequest.prependUrl(codeloadBaseUrl))
-  )
-
-  const downloadExample = (config: ExampleConfig) =>
-    HttpClientRequest.get("/Effect-TS/examples/tar.gz/main").pipe(
-      codeloadClient,
-      HttpClientResponse.stream,
-      Stream.run(NodeSink.fromWritable(() =>
-        Tar.extract({
-          cwd: config.projectName,
-          strip: 2 + config.projectType.example.split("/").length,
-          filter: (path) => path.includes(`examples-main/examples/${config.projectType.example}`)
-        }), () => ValidationError.invalidValue(HelpDoc.p(`Failed to download example ${config.projectType.example}`))))
+    const codeloadClient = client.pipe(
+      HttpClient.filterStatusOk,
+      HttpClient.mapRequest(HttpClientRequest.prependUrl(codeloadBaseUrl))
     )
 
-  const downloadTemplate = (config: TemplateConfig) =>
-    HttpClientRequest.get("/Effect-TS/examples/tar.gz/main").pipe(
-      codeloadClient,
-      HttpClientResponse.stream,
-      Stream.run(NodeSink.fromWritable(
-        () =>
-          Tar.extract({
-            cwd: config.projectName,
-            strip: 2 + config.projectType.template.split("/").length,
-            filter: (path) => path.includes(`examples-main/templates/${config.projectType.template}`)
-          }),
-        () => ValidationError.invalidValue(HelpDoc.p(`Failed to download template ${config.projectType.template}`))
-      ))
-    )
+    const downloadExample = (config: ExampleConfig) =>
+      codeloadClient.get("/Effect-TS/examples/tar.gz/main").pipe(
+        HttpClientResponse.stream,
+        Stream.run(NodeSink.fromWritable(
+          () =>
+            Tar.extract({
+              cwd: config.projectName,
+              strip: 2 + config.projectType.example.split("/").length,
+              filter: (path) => path.includes(`examples-main/examples/${config.projectType.example}`)
+            }),
+          () => ValidationError.invalidValue(HelpDoc.p(`Failed to download example ${config.projectType.example}`))
+        ))
+      )
 
-  return {
-    downloadExample,
-    downloadTemplate
-  } as const
-})
+    const downloadTemplate = (config: TemplateConfig) =>
+      codeloadClient.get("/Effect-TS/examples/tar.gz/main").pipe(
+        HttpClientResponse.stream,
+        Stream.run(NodeSink.fromWritable(
+          () =>
+            Tar.extract({
+              cwd: config.projectName,
+              strip: 2 + config.projectType.template.split("/").length,
+              filter: (path) => path.includes(`examples-main/templates/${config.projectType.template}`)
+            }),
+          () => ValidationError.invalidValue(HelpDoc.p(`Failed to download template ${config.projectType.template}`))
+        ))
+      )
 
-export class GitHub extends Effect.Tag("app/GitHub")<
-  GitHub,
-  Effect.Effect.Success<typeof make>
->() {
-  static Live = Layer.effect(this, make)
-}
+    return {
+      downloadExample,
+      downloadTemplate
+    } as const
+  })
+}) {}
