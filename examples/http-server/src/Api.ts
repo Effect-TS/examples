87c050e009f2d5687c8b8d8ff1a1b07093385236
Bram Borggreve <beeman@beeman.nl>
2025-08-21 14:11:48 +0100

    fix: linting error in http-server example

--- examples/http-server/src/Api.ts
+++ examples/http-server/src/Api.ts
@@ -3,7 +3,7 @@ import { AccountsApi } from "./Accounts/Api.js"
 import { GroupsApi } from "./Groups/Api.js"
 import { PeopleApi } from "./People/Api.js"
 
-export class Api extends HttpApi.make('api')
+export class Api extends HttpApi.make("api")
   .add(AccountsApi)
   .add(GroupsApi)
   .add(PeopleApi)
